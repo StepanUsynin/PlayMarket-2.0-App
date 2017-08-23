bcb69cfe8d918039fe935ce548d4e1806faca6a7
Sam Sheffres <ssheffres@gmail.com>
2017-08-23 15:56:57 +0700

    Add Video Player

--- app/src/main/java/com/blockchain/store/playstore/IntroLogoActivity.java
+++ app/src/main/java/com/blockchain/store/playstore/IntroLogoActivity.java
@@ -1,13 +1,51 @@
 package com.blockchain.store.playstore;
 
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class IntroLogoActivity extends AppCompatActivity {
+
+    TextView logoTextView;
+    VideoView logoVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_logo);
+
+        initViewVariables();
+        setupAndPlayVideo();
+    }
+
+    protected void initViewVariables() {
+        logoTextView = (TextView) findViewById(R.id.LogoTextView);
+        logoVideoView = (VideoView) findViewById(R.id.LogoVideoView);
+    }
+
+    protected void setLogoTextFont() {
+        Typeface tf = Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/roboto.ttf");
+        logoTextView.setTypeface(tf);
+    }
+
+    protected void setupAndPlayVideo() {
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.image;
+        logoVideoView.setVideoURI(Uri.parse(path));
+
+        logoVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+        logoVideoView.start();
+
     }
 }
