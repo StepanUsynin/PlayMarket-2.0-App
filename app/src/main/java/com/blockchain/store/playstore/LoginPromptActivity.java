52ba1493de79ab2461d783966c7295a3cc9f1d68
Sam Sheffres <ssheffres@gmail.com>
2017-08-24 08:57:45 +0700

    Add New User Welcome Activity

--- app/src/main/java/com/blockchain/store/playstore/LoginPromptActivity.java
+++ app/src/main/java/com/blockchain/store/playstore/LoginPromptActivity.java
@@ -1,7 +1,9 @@
 package com.blockchain.store.playstore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPromptActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class LoginPromptActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_prompt);
     }
+
+    public void LoadNewUserWelcomeActivity(View view) {
+        Intent myIntent=new Intent(getApplicationContext(),NewUserWelcomeActivity.class );
+        startActivityForResult(myIntent,0);
+    }
+
 }
---
+++ app/src/main/java/com/blockchain/store/playstore/NewUserWelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.blockchain.store.playstore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewUserWelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_welcome);
+    }
+}
