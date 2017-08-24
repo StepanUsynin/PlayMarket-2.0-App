bda8504ddcc53e4e9604b4c8ad15d15ededd3235
Sam Sheffres <ssheffres@gmail.com>
2017-08-24 10:22:32 +0700

    Setup Basic Etherium Node, Generate and Display Address

--- app/src/main/java/com/blockchain/store/playstore/NewUserWelcomeActivity.java
+++ app/src/main/java/com/blockchain/store/playstore/NewUserWelcomeActivity.java
@@ -1,14 +1,65 @@
 package com.blockchain.store.playstore;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import io.ethmobile.ethdroid.EthDroid;
+import io.ethmobile.ethdroid.KeyManager;
 
 public class NewUserWelcomeActivity extends AppCompatActivity {
+
+    public KeyManager keyManager;
+    private String datadir;
+    private String etherAddress;
+    private TextView AddressTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_welcome);
+
+        setupView();
+        setDatadir();
+        startEtherNode();
+        setupKeyManager();
+    }
+
+    protected void setupView() {
+        AddressTextView = (TextView) findViewById(R.id.AddressTextView);
+    }
+
+    protected void setupKeyManager() {
+        keyManager = KeyManager.newKeyManager(datadir);
+
+        try {
+            keyManager.newAccount("Test");
+            etherAddress = keyManager.getAccounts().get(0).getAddress().getHex().toString();
+            Log.d("Ether", etherAddress);
+
+            AddressTextView.setText(etherAddress);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void setDatadir() {
+        datadir = getFilesDir().getAbsolutePath();
+    }
+
+    protected void startEtherNode() {
+        try {
+            new EthDroid.Builder(datadir)
+                    .onTestnet()
+                    .build()
+                    .start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
